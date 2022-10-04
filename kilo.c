b3728e44acc137275cd0adc001f1d6b7eb82a07b
Kaleb Petersen <kcpetersen111@gmail.com>
2022-10-03 22:25:53 -0600

    forgot to add this before

--- kilo.c
+++ kilo.c
@@ -1,20 +1,33 @@
 #include "unistd.h"
 #include "stdio.h"
 #include "termios.h"
+#include "stdlib.h"
+
+struct termios orig_termios;
+
+void disableRawMode(){
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
 
 void enableRawMode(){
+	// struct termios raw;
 
+	tcgetattr(STDIN_FILENO, &orig_termios);
+
+	atexit(disableRawMode);
+	struct termios raw = orig_termios;
+
+	raw.c_lflag &= ~(ECHO);
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
 int main(){
-	printf("Hello, World!\n");
 
 	enableRawMode();
 
 	char c;
 
-	while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q'){
-		printf("%c\n",c);
-	}
+	while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+	
 	return 0;
 }
